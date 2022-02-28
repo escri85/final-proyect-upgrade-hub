b1a299e60f3a36c3125d29e7681d2bb936351091
Hector Escolante <escolante.enriquez@bootcamp-upgrade.com>
2022-02-28 16:52:20 +0100

    Refactoring components, adding the home route and giving the first brushstrokes to the home section

--- front-project/src/App.jsx
+++ front-project/src/App.jsx
@@ -1,24 +1,21 @@
-import './App.scss';
-import Header from './components/Header/Header';
-
-
-//Router-dom
+import { Gallery, Footer, Header } from './components';
+import Home from './pages/Home/Home';
 import {
   Routes,
   Route,
 } from "react-router-dom";
 
-//Componentes
-import Gallery from './components/Gallery/Gallery';
-import Footer from './components/Footer/Footer';
-
+import './App.scss';
 
 function App() {
   return (
     <div className="App">
       <Header/>
       <Routes>
-        <Route path="/" element={<Gallery/>}/>
+        <Route path='/'>
+          <Route path="/" element={<Home/>}/>
+          <Route path='/gallery' element={<Gallery/>}/>
+        </Route>
       </Routes>
       <Footer/>
     </div>
