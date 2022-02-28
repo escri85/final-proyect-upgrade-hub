7e6985576b892cc1300fa2e13fcb9a05cac0ea11
luisPabloDRM <luis.pablo@bootcamp-upgrade.com>
2022-02-28 12:28:47 +0100

    FEAT: Adding simple without icons Fotter

--- front-project/src/components/Footer/Footer.jsx
+++ front-project/src/components/Footer/Footer.jsx
@@ -1,8 +1,28 @@
-import 'Footer.scss';
+import './Footer.scss' 
+
+
 
 const Footer = () => {
     return (
-        <div>Footer</div>
+
+
+        <footer class="footer-distributed">
+        <div class="footer-left">
+          <h3>
+            Upgrade<span>Store</span>
+          </h3>
+  
+          <p class="footer-company-name"> 2022</p>
+        </div>
+  
+        <div class="footer-right">
+          <p class="footer-company-about">
+            iconos RRSS
+          </p>
+        </div>
+      </footer>
+  
+
     )
 }
 
