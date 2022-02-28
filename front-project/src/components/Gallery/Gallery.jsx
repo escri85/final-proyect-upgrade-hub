8d2cc9a4bf65d2edd831ea0d5830d6a83de7ca3e
Jose AC <joseralmendros@gmail.com>
2022-02-28 12:50:34 +0100

    FEAT: proob-gallery

--- front-project/src/components/Gallery/Gallery.jsx
+++ front-project/src/components/Gallery/Gallery.jsx
@@ -1,3 +1,4 @@
+import './Gallery.scss';
 import { useEffect } from 'react';
 import { connect } from 'react-redux';
 import { getToApi } from '../../redux/actions/apiActions';
@@ -10,10 +11,36 @@ const Gallery = (props) => {
           // eslint-disable-next-line react-hooks/exhaustive-deps
     }, []);
     
-    return (<div>
-
-        
-
+    return (<div className="container">
+        {
+            props.data.map(product => 
+                    <div className="el-wrapper">
+                    <div className="box-up">
+                        <img className="img" src={product.image} alt=""/>
+                        <div className="img-info">
+                        <div className="info-inner">
+                            <span className="p-name">{product.title}</span>
+                            <span className="p-company">{product.categorie}</span>
+                        </div>
+                            <div className="a-size">{product.description}</div>
+                        </div>
+                    </div>
+                
+                        <div className="box-down">
+                        <div className="h-bg">
+                            <div className="h-bg-inner"></div>
+                        </div>
+            
+                        <a className="cart" href="#">
+                        <span className="price">{product.price}€</span>
+                        <span className="add-to-cart">
+                            <span className="txt">Añadir al carrito</span>
+                        </span>
+                        </a>
+                    </div>
+                    </div>
+            )
+        }
     </div>)
 
 }
--- front-project/src/index.js
+++ front-project/src/index.js
@@ -5,7 +5,7 @@ import App from './App';
 import { store } from './redux/store';
 import { Provider } from 'react-redux';
 import {BrowserRouter as Router} from 'react-router-dom';
-
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(
   <React.StrictMode>
