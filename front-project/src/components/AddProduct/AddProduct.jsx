3034808e01b766c87ab1a4e7ed98cef24f19e296
Sergio Escribano Fernandez <escri85@gmail.com>
2022-03-07 00:10:50 +0100

    FEAT: create spinner and logic

--- front-project/src/components/AddProduct/AddProduct.jsx
+++ front-project/src/components/AddProduct/AddProduct.jsx
@@ -4,8 +4,9 @@ import { InputText } from "primereact/inputtext";
 import { InputTextarea } from "primereact/inputtextarea";
 import axios from "axios";
 import { Button } from "primereact/button";
-import { Dropdown } from 'primereact/dropdown';
+import { Dropdown } from "primereact/dropdown";
 import swal from "sweetalert";
+import { ProgressSpinner } from 'primereact/progressspinner';
 import "./AddProduct.scss";
 const AddProduct = () => {
   const INITIAL_STATE = {
@@ -18,10 +19,9 @@ const AddProduct = () => {
   };
 
   const [newProduct, setNewProduct] = useState(INITIAL_STATE);
-  const [catego,setCatego] = useState({});
-const [routesCategorie,setRoutescategorie] = useState('')
-
- 
+  const [catego, setCatego] = useState({});
+  const [routesCategorie, setRoutescategorie] = useState("");
+  const [spinner,setSpinner]=useState(false)
   const handleChange = (event) => {
     const names = event.target.name;
     const values = event.target.value;
@@ -30,7 +30,7 @@ const [routesCategorie,setRoutescategorie] = useState('')
       [names]: values,
     });
   };
-  const postApi =  () => {
+  const postApi = () => {
     const addProduct = {
       title: newProduct.title,
       price: newProduct.price,
@@ -38,118 +38,128 @@ const [routesCategorie,setRoutescategorie] = useState('')
       description: newProduct.description,
       stock: newProduct.stock,
       image: newProduct.image,
-    }
-   console.log(addProduct);
-     const sendProduct =  axios.post(`http://localhost:4000/${routesCategorie}`, addProduct);
-      
-   console.log('sendproduct',routesCategorie);
+    };
+    console.log(addProduct);
+    const sendProduct = axios.post(
+      `http://localhost:4000/${routesCategorie}`,
+      addProduct
+    );
+
+    console.log("sendproduct", routesCategorie);
     console.log(
       "el producto se envio desde el front correctamente",
       sendProduct
     );
   };
+  
   const onSubmit = (event) => {
     event.preventDefault();
-    swal("Producto subido!!");
+
+    setSpinner(true)
+setTimeout(()=>{
+  swal("Producto subido!!");
+  setSpinner(false)
+
+},3000)
     postApi();
   };
-  const onCityChange = (event) => {
+  const onCategorieChange = (event) => {
     console.log(event.value.name);
-    const values = event.value
+    const values = event.value;
     setCatego(values);
     console.log(catego);
     switch (catego.name) {
-      case 'Accesorios':
-        setRoutescategorie('accesories')
+      case "Accesorios":
+        setRoutescategorie("accesories");
         break;
-        case "Ropa para hombre":
-        setRoutescategorie('manproducts')
+      case "Ropa para hombre":
+        setRoutescategorie("manproducts");
         break;
-        // case "Complementos":
-        // setRoutescategorie('accesories')
-        // break;
-        case "Ropa para mujer":
-        setRoutescategorie('woman')
+      // case "Complementos":
+      // setRoutescategorie('accesories')
+      // break;
+      case "Ropa para mujer":
+        setRoutescategorie("woman");
         break;
-        case "Sneakers":
-        setRoutescategorie('sneakers')
+      case "Sneakers":
+        setRoutescategorie("sneakers");
         break;
       default:
         break;
     }
-}
-const categories = [
-  { name: 'Accesorios'},
-  { name:  "Ropa para hombre" },
-  { name: "Complementos"},
-  { name: "Sneakers"},
-  { name:  "Ropa para mujer"}
-];
+  };
+  const categories = [
+    { name: "Accesorios" },
+    { name: "Ropa para hombre" },
+    { name: "Complementos" },
+    { name: "Sneakers" },
+    { name: "Ropa para mujer" },
+  ];
   return (
     <div className="addProduct">
       <div className="addProduct__edit">
         <h1>Edita tu articulo</h1>
-        <form action="" onSubmit={onSubmit} className="form">
-          <label htmlFor="categorias">Categoria del producto</label>
-          <Dropdown
-          id="categorias"
-            value={catego}
-            options={categories}
-            onChange={onCityChange}
-            optionLabel="name"
-            placeholder="Elige una categoria"
-          />
-          <label htmlFor="titulo">Pon un nombre a tu articulo</label>
-          <InputText
-            id="titulo"
-            onChange={handleChange}
-            placeholder="articulo"
-            name="title"
-          />
-          <label htmlFor="stacked">Pon un precio</label>
-
-          <InputNumber
-            inputId="stacked"
-            onValueChange={handleChange}
-            showButtons
-            mode="currency"
-            currency="EUR"
-            name="price"
-          />
-          <label htmlFor="descripcion">Descripcion</label>
+        <form action="" onSubmit={onSubmit}>
+          <div className="categoria">
+            {/* <label htmlFor="categorias">Categoria del producto</label> */}
+            <Dropdown
+              id="categorias"
+              value={catego}
+              options={categories}
+              onChange={onCategorieChange}
+              optionLabel="name"
+              placeholder="Elige una categoria"
+            />
+            {/* <label htmlFor="titulo">Pon un nombre a tu articulo</label> */}
+            <InputText
+              id="titulo"
+              onChange={handleChange}
+              placeholder="articulo"
+              name="title"
+            />
+          </div>
+          <div className="categoria">
+            <InputNumber
+              inputId="stacked"
+              onValueChange={handleChange}
+              mode="currency"
+              currency="EUR"
+              name="price"
+              placeholder="Precio"
+            />
+            <InputNumber
+              id="stock"
+              inputId="minmax-buttons"
+              onValueChange={handleChange}
+              mode="decimal"
+              min={0}
+              max={100}
+              name="stock"
+              placeholder="Unidades"
+            />
+          </div >
+          <div className="categoria">
           <InputTextarea
             id="descripcion"
             onChange={handleChange}
-            rows={5}
-            cols={30}
+            rows={3}
+            cols={50}
             autoResize
             placeholder="Describe brevemente tu articulo"
             name="description"
           />
-          <label htmlFor="stock">Cuantos articulos vas a vender</label>
-
-          <InputNumber
-            id="stock"
-            inputId="minmax-buttons"
-            onValueChange={handleChange}
-            mode="decimal"
-            showButtons
-            min={0}
-            max={100}
-            name="stock"
-          />
-          <label htmlFor="url">Pon tu direccion de imagen</label>
+          </div>
           <InputText
             id="url"
             onChange={handleChange}
-            placeholder="imagen"
+            placeholder="url de imagen"
             name="image"
           />
-          <Button label="Submit" />
+          <Button  label="+ Subir Producto" />
+          {spinner===true ? <ProgressSpinner /> : undefined}
         </form>
       </div>
       <div className="addProduct__confirm">
-        <h1>Asi se vera en nuestra pagina</h1>
         <div className="el-wrapper">
           <div className="box-up">
             <img className="img" src={newProduct.image} alt="" />
