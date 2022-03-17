9ec6636c79e6f783ce777c7450f8c2829055b889
Jose AC <joseralmendros@gmail.com>
2022-03-17 18:30:29 +0100

    FEAT: some header

--- front-project/src/components/Header/Header.jsx
+++ front-project/src/components/Header/Header.jsx
@@ -14,17 +14,14 @@ const Header = ({toggleTheme}) => {
 }
   return (
     <div className="header">
-      
-     
-        <Link to='/'>
-          <h1>UP-Store</h1>
-        </Link>
-        <div className="__swicth">
-       <label htmlFor="darkMode">{checked1===true ? 'Dark' : 'Light'}</label>
-
-        <InputSwitch checked={checked1} name='darkMode' onChange={handleChange}/>
-        </div>
-    
+      <div className="header__title">
+          <img src="https://cdn.discordapp.com/attachments/954061730814787637/954061759789015090/Captura_de_pantalla_2022-03-17_a_las_17.59.21.png" alt="logo" />
+          <Link className="header__text" to='/'>Connect Shop</Link>  
+      </div>
+      <div className="__swicth">
+          <label htmlFor="darkMode">{checked1===true ? 'Dark' : 'Light'}</label>
+          <InputSwitch checked={checked1} name='darkMode' onChange={handleChange}/>
+          </div>
       <Navbar />
     </div>
   );
