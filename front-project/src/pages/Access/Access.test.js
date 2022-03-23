555b4800e982fd6e69d7a2b0bdc890f6736a7d92
Sergio Escribano Fernandez <escri85@gmail.com>
2022-03-24 00:19:48 +0100

    FEAT:add test in man clthes page passed call api by id item

--- front-project/src/pages/Access/Access.test.js
+++ front-project/src/pages/Access/Access.test.js
@@ -1,18 +1,19 @@
 import React from "react";
 import '@testing-library/jest-dom/extend-expect'
-import{render, screen,fireEvent,waitFor }from '@testing-library/react'
+import{render, screen,waitFor }from '@testing-library/react'
 import Access from "./Access";
 import { connect } from "formik";
 
+const setup =()=>render(connect(<Access/>))
 
 // beforeEach(()=>{
-//     render(<Access/> )
+//     setup()
 //   })
 
 
   
 test('render titles component Access',async()=>{
-    render(connect(<Access/>) )
+    setup()
 
     const inicioSesion = screen.findAllByText(/iniciar sesion/i)
     await waitFor(()=>expect(inicioSesion))
