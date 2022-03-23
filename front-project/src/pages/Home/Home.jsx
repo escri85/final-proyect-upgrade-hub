e9f6141018d907e8ed7e8d53e571db7887dde888
Hector <89035945+hectoresen@users.noreply.github.com>
2022-03-23 20:19:09 +0100

    FEAT: Working on the home page

--- front-project/src/pages/Home/Home.jsx
+++ front-project/src/pages/Home/Home.jsx
@@ -1,17 +1,32 @@
-
-import Carousel from 'react-bootstrap/Carousel'
-import { Search } from '../../components';
-
+import { HelpModule, Search } from '../../components';
+import { TabView, TabPanel } from 'primereact/tabview';
+import { useState } from 'react';
+import { Button } from 'primereact/button';
 
 import './Home.scss';
 
 const Home = () =>{
+const [activeIndex, setActiveIndex] = useState(0)
 
-   
-   
     return (
         <div className='home' >
-            <Search/>
+            <div className='home__search'>
+                <TabView activeIndex={activeIndex} onTabChange={(e) => setActiveIndex(e.index)}>
+                    <TabPanel className='home__search-tabpanel' header="Busca tus productos">
+                        <div className='home__search__tabpanel1'>
+                            <Search/>
+                        </div>
+                    </TabPanel>
+                    <TabPanel className="home__search-tabpanel" header="Ofertas de temporada">
+                        <div className='home__search-tabpanel2'>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corrupti sint deserunt, temporibus repudiandae expedita vitae nam, quae amet debitis facere velit laborum beatae, nobis officia nihil aliquid natus? Praesentium, harum.</p>
+                        </div>
+                    </TabPanel>
+                    <TabPanel className='home__search-tabpanel' header="Ayuda">
+                        <HelpModule/>
+                    </TabPanel>
+                </TabView>
+            </div>
         </div>
     )
 }
