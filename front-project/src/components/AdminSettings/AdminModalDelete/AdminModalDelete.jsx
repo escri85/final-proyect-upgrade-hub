a53214248fa4048b0bfe3ba2cb588a60b33467b9
Luis Pablo <luispablo@MacBook-Air-de-Luis.local>
2022-03-27 17:26:18 +0200

    FEAT Some translations

--- front-project/src/components/AdminSettings/AdminModalDelete/AdminModalDelete.jsx
+++ front-project/src/components/AdminSettings/AdminModalDelete/AdminModalDelete.jsx
@@ -1,6 +1,7 @@
 import React, { useContext, useState } from 'react';
 import { Row, Text, Input, Button, Modal, Checkbox, Avatar } from '@nextui-org/react';
 import { AdminContext } from '../../../Contexts/AdminContext';
+import { FormattedMessage  as T} from 'react-intl';
 
 const AdmingModalSettings = () => {
     const [visible, setVisible] = useState(true);
@@ -24,20 +25,20 @@ const AdmingModalSettings = () => {
         <Modal.Header>
             <Text id="modal-title" size={18}>
             <Text b size={18}>
-                Eliminar producto
+                <T id="AdminSettings.Stock.Delete" />
             </Text>
             </Text>
         </Modal.Header>
         <Modal.Body>
             <Row justify="space-between">
             <Text color="error" size={14}>
-                Una vez eliminado no podrás volver atrás
+            <T id="AdminSettings.Stock.Update" />
             </Text>
             </Row>
         </Modal.Body>
         <Modal.Footer>
             <Button auto onClick={(deleteProduct)}>
-            Eliminar producto
+            <T id="AdminSettings.Stock.Delete" />
             </Button>
         </Modal.Footer>
     </Modal>
