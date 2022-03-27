f70f6e48cbd2671bc6e8959dfaa4a29a3cfb1ff8
Luis Pablo <luispablo@MacBook-Air-de-Luis.local>
2022-03-27 19:07:39 +0200

    FEAT: Finishing sub-subnavabar

--- front-project/src/pages/Home/Home.jsx
+++ front-project/src/pages/Home/Home.jsx
@@ -3,6 +3,8 @@ import { TabView, TabPanel } from "primereact/tabview";
 import { useState } from "react";
 import "./Home.scss";
 import CarouselContainer from "./Carousel/CarouselContainer";
+import { InputText } from 'primereact/inputtext';
+import { Button } from 'primereact/button';
 
 import Map from "../../components/GoogleMaps/Map";
 
@@ -17,18 +19,12 @@ const Home = ({productsResult}) => {
     <CarouselContainer />
       <div className="home">
         <div className="home__search">
+
           <TabView
             style={{marginTop:'-20px'}}
             activeIndex={activeIndex}
             onTabChange={(e) => setActiveIndex(e.index)}
           >
-            <TabPanel
-              className="home__search-tabpanel"
-              header="Busca tus productos"
-            >
-              <div className="home__search__tabpanel1">
-              </div>
-            </TabPanel>
             <TabPanel
               className="home__search-tabpanel"
               header="Ofertas de temporada"
@@ -42,6 +38,22 @@ const Home = ({productsResult}) => {
                 <img src="https://bio-grancanaria.com/wp-content/uploads/2019/07/newsletter-logo.png" alt="newslater" />
               </div>
             </TabPanel>
+            <TabPanel
+              className="home__search-tabpanel"
+              header="Haznos sugerencias!"
+            >
+              <div className="home__search__tabpanel1">
+              <div className='access__forms-register-input'>
+                    <span className="p-float-label p-input-icon-right">
+                        <i className="pi pi-envelope" />
+                            <InputText id="text" name="text" type="text" ></InputText>
+                        <label htmlFor="email">Añadir comentario</label>
+                    </span>
+                        <Button type="submit" label="Enviar!  " className="mt-2" />
+            </div>
+              </div>
+            </TabPanel>
+            
             <TabPanel className="home__search-tabpanel" header="Ayuda">
               <HelpModule />
             </TabPanel>
@@ -49,8 +61,8 @@ const Home = ({productsResult}) => {
         </div>
         <div className="home__contain">
           <div className="home__contain-info">
-            <p>¿No conoces nuestras instalaciones?</p>
-          </div>
+            
+            </div>
           <div className="home__contain-map"></div>
         </div>
       </div>
